65007e3e2055edd205f4ca5429cb5b8f74ac6ad7
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-06 09:35:43 +0530

    [Pradip]REFACTORED-Refactored with generic type to find maximum

--- TestMaximum/src/test/java/com/bridgelabz/moodanalyzer/TestMaximum/MaximumTest.java
+++ TestMaximum/src/test/java/com/bridgelabz/moodanalyzer/TestMaximum/MaximumTest.java
@@ -1,67 +1,66 @@
 package com.bridgelabz.moodanalyzer.TestMaximum;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MaximumTest {
+
     Maximum maximum = new Maximum();
 
     @Test
-    public void givenInteger_WhenFirstPosition_ShouldReturnMaxInt() {
-        Assert.assertEquals(8, maximum.findMaximum(6, 8, 1));
+    public void givenMaxInteger_WhenFirstPosition_ShouldReturnMaxInt() {
+
+        int max = maximum.findMaximum(3,1,2);
+        Assert.assertEquals(3, max);
     }
 
     @Test
-    public void givenInteger_WhenSecondPosition_ShouldReturnMaxInt() {
-        Assert.assertEquals(34, maximum.findMaximum(6, 34, 1));
+    public void givenMaxInteger_WhenSecondPosition_ShouldReturnMaxInt() {
+        int max = maximum.findMaximum(3,8,2);
+        Assert.assertEquals(8, max);
     }
 
     @Test
-    public void givenInteger_WhenThirdPosition_ShouldReturnMaxInt() {
-        Assert.assertEquals(6, maximum.findMaximum(4, 2, 6));
+    public void givenMaxInteger_WhenThirdPosition_ShouldReturnMaxInt() {
+        int max = maximum.findMaximum(3,8,12);
+        Assert.assertEquals(12, max);
     }
 
     @Test
-    public void givenInteger_WhenMore_ShouldReturnMaxInt() {
-        Assert.assertEquals(8, maximum.findMaximum(4, 2, 6, 7, 8, 2));
+    public void givenMaxFloat_WhenFirstPosition_ShouldReturnMaxFloat() {
+
+        float max = maximum.findMaximum(51.4f, 50.2f,9.5f);
+        Assert.assertEquals(51.4f, max, 0.0f);
     }
 
     @Test
-    public void givenFloat_WhenFirstPosition_ShouldReturnMaxFloat() {
-        Assert.assertEquals(5.5f, maximum.findMaximum(5.5f, 2.5f, 2.4f));
+    public void givenMaxFloat_WhenSecondPosition_ShouldReturnMaxFloat() {
+
+        float max = maximum.findMaximum(51.4f, 52.2f,9.5f);
+        Assert.assertEquals(52.2f, max, 0.0f);
     }
 
     @Test
-    public void givenFloat_WhenSecondPosition_ShouldReturnMaxFloat() {
-        Assert.assertEquals(16.5f, maximum.findMaximum(5.5f, 16.5f, 2.4f));
+    public void givenMaxFloat_WhenThirdPosition_ShouldReturnMaxFloat() {
+        float max = maximum.findMaximum(1.4f, 5.2f,9.5f);
+        Assert.assertEquals(9.5f, max, 0.0f);
     }
 
     @Test
-    public void givenFloat_WhenThirdPosition_ShouldReturnMaxFloat() {
-        Assert.assertEquals(23.4f, maximum.findMaximum(5.5f, 16.5f, 23.4f));
+    public void givenMaxString_WhenFirstPosition_ShouldReturnMaxString() {
+        String max = maximum.findMaximum("apple","Peach","Banana");
+        Assert.assertEquals("apple", max);
     }
 
     @Test
-    public void givenFloat_WhenMoreValues_ShouldReturnMaxFloat() {
-        Assert.assertEquals(80.4f, maximum.findMaximum(5.5f, 16.5f, 23.4f, 5.6f, 80.4f, 23.5f));
+    public void givenMaxString_WhenSecondPosition_ShouldReturnMaxString() {
+        String max = maximum.findMaximum("Apple","Peach","Banana");
+        Assert.assertEquals("Peach", max);
     }
 
     @Test
-    public void givenString_WhenFirstPosition_ShouldReturnMaxString() {
-        Assert.assertEquals("apple", maximum.findMaximum("apple", "Peach", "Banana"));
-    }
-
-    @Test
-    public void givenString_WhenSecondPosition_ShouldReturnMaxString() {
-        Assert.assertEquals("Peach", maximum.findMaximum("Apple", "Peach", "Banana"));
-    }
-
-    @Test
-    public void givenString_WhenThirdPosition_ShouldReturnMaxString() {
-        Assert.assertEquals("banana", maximum.findMaximum("Apple", "Peach", "banana"));
-    }
-
-    @Test
-    public void givenString_WhenMoreValues_ShouldReturnMaxString() {
-        Assert.assertEquals("apple", maximum.findMaximum("apple", "Peach", "Banana", "Lion", "Tiger"));
+    public void givenMaxString_WhenThirdPosition_ShouldReturnMaxString() {
+        String max = maximum.findMaximum("Apple","Peach","banana");
+        Assert.assertEquals("banana", max);
     }
 }
