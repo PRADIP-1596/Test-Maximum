109394c8eef4054e309e13e8c3cc3c4f69dc8a27
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-06 10:35:59 +0530

    REFACTORED-Extended findMaximum method to take more values

--- TestMaximum/src/main/java/com/bridgelabz/moodanalyzer/TestMaximum/Maximum.java
+++ TestMaximum/src/main/java/com/bridgelabz/moodanalyzer/TestMaximum/Maximum.java
@@ -1,20 +1,22 @@
 package com.bridgelabz.moodanalyzer.TestMaximum;
-public class Maximum {
+import java.util.Arrays;
+
+
+public class Maximum<E extends Comparable<E>> {
+    public Maximum() {
+
+    }
+
     /**
-     * generic method for returning maximum value
-     *
-     * @param firstValue
-     * @param secondValue
-     * @param thirdValue
+     * @param val
      * @param <E>
      * @return
      */
-    public <E extends Comparable<E>> E findMaximum(E firstValue, E secondValue, E thirdValue) {
-        if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
-            return firstValue;
-        else if (secondValue.compareTo(thirdValue) > 0 && secondValue.compareTo(firstValue) > 0)
-            return secondValue;
-        else
-            return thirdValue;
+    public <E extends Comparable<E>> E findMaximum(E... val) {
+
+        E[] value = val;
+        int length = value.length;
+        Arrays.sort(value);
+        return value[length - 1];
     }
 }
