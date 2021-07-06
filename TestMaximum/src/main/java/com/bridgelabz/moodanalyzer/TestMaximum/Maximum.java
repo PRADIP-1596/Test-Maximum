b945a8a7a77ed8eb3c784b4e5a8c7c0df7ed1bdb
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-06 09:29:00 +0530

    [Pradip]:ADDED Code In Master Will Check Maximum Number

---
+++ TestMaximum/src/main/java/com/bridgelabz/moodanalyzer/TestMaximum/Maximum.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.moodanalyzer.TestMaximum;
+
+import java.util.Arrays;
+
+public class Maximum<E> {
+    /**
+     * Method to sort and find out maximum value from array
+     *
+     * @param value
+     * @return
+     */
+    public E findMaximum(E... value) {
+        int length = value.length;
+        Arrays.sort(value);
+        E max = value[length - 1];
+        System.out.println(max);
+        return max;
+    }
+
+}
